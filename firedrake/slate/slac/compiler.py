4a5056a7d7804053ca2f5f1c7b34ebf9e84f573d
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2020-08-24 16:52:46 +0200

    Don't produce C-code in Slate compiler anymore, give loopy program back.

--- firedrake/slate/slac/compiler.py
+++ firedrake/slate/slac/compiler.py
@@ -151,11 +151,7 @@ def generate_loopy_kernel(slate_expr, tsfc_parameters=None):
     loopy_merged = loopy.register_function_id_to_in_knl_callable_mapper(loopy_merged, inv_fn_lookup)
     loopy_merged = loopy.register_function_id_to_in_knl_callable_mapper(loopy_merged, solve_fn_lookup)
 
-    # WORKAROUND: Generate code directly from the loopy kernel here,
-    # then attach code as a c-string to the op2kernel
-    code = loopy.generate_code_v2(loopy_merged).device_code()
-    code.replace('void slate_kernel', 'static void slate_kernel')
-    loopykernel = op2.Kernel(code, loopy_merged.name, ldargs=["-llapack"])
+    loopykernel = op2.Kernel(loopy_merged, loopy_merged.name)
 
     kinfo = KernelInfo(kernel=loopykernel,
                        integral_type="cell",  # slate can only do things as contributions to the cell integrals
